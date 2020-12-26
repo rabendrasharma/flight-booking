b98f58b0135b52ed3a53dbcdda958edb9b5b4fcd
Rabendra Sharma <rabendrasharma007@gmail.com>
2020-12-26 10:02:24 +0530

    Pnr generatioon code added

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,10 @@ class Ticket < ApplicationRecord
   belongs_to :reservation
   belongs_to :route
   belongs_to :booked_by, :class_name => "User", :foreign_key => "booked_by", optional: true
+  before_save :generate_pnr_number
+
+  #Code to generate unique pnr number per seat basis, with selected seat type
+  def generate_pnr_number
+    self.pnr_number = "#{self.seat_type}-#{SecureRandom.base36(6)}"
+  end
 end
